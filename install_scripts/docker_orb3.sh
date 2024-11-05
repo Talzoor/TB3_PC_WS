26578b8d11db0e16344d82fcd02d3300c4b4c791
tal <tal@Ubuntu22.04v1124>
2024-11-05 11:55:12 +0200

    commit 24_11_05 update1

--- install_scripts/docker_orb3.sh
+++ install_scripts/docker_orb3.sh
@@ -2,6 +2,7 @@
 
 RED="\e[31m"
 GREEN="\e[32m"
+LGREEN="\e[92m"
 BLUE="\e[34m"
 LBLUE="\e[94m"
 WHITE="\e[97m"
@@ -32,22 +33,66 @@ if [ ! -f $filePath ]; then
 fi
 
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# echo "Script directory: $SCRIPT_DIR"
+VAR_FILE_PATH=$SCRIPT_DIR/variables.sh
+source $VAR_FILE_PATH
+
+ORB_DOCKER_CNAME=$ORB_DOCKER_CNAME
+ORB_DOCKER_VER=$ORB_DOCKER_VER
+
+echo -e "${BOLDGREEN}\n ORB_DOCKER_CNAME=${BOLDWHITE}${ORB_DOCKER_CNAME}${ENDCOLOR} ${ENDCOLOR}"
+echo -e "${BOLDGREEN} ORB_DOCKER_VER=${BOLDWHITE}${ORB_DOCKER_VER}${ENDCOLOR} ${ENDCOLOR}"
+echo -e "${BOLDWHITE} \n\t\t\tChange it? [N/y] ${ENDCOLOR}:"
+
+read -p "" ans
+ans=${ans:-n}
+declare -l ans_lower=${ans}
+# echo -e "${BOLDGREEN} answer = ${ans_lower} ${ENDCOLOR}"
+
+if [ $ans_lower = "n" ]; then
+    echo -e "${BOLDGREEN} ${ENDCOLOR}"
+else
+    echo -e "${LGREEN} ORB_DOCKER_CNAME ? [CURRENT/new]${ENDCOLOR}"
+    read -p "" ORB_DOCKER_CNAME_new
+    ORB_DOCKER_CNAME_new=${ORB_DOCKER_CNAME_new:-$ORB_DOCKER_CNAME}
+    
+    echo -e "${LGREEN} ORB_DOCKER_VER ? [CURRENT/new]${ENDCOLOR}"
+    read -p "" ORB_DOCKER_VER_new
+    ORB_DOCKER_VER_new=${ORB_DOCKER_VER_new:-$ORB_DOCKER_VER}
+    
+    rm -rf $VAR_FILE_PATH
+    echo "#!/bin/bash" > $VAR_FILE_PATH
+    echo "" >> $VAR_FILE_PATH
+    printf "echo -e \"\\\n\t---Setting vars from 'variables.sh'---\"" >> $VAR_FILE_PATH
+    echo "" >> $VAR_FILE_PATH
+    echo "export ORB_DOCKER_CNAME=${ORB_DOCKER_CNAME_new}" >> $VAR_FILE_PATH
+    echo "export ORB_DOCKER_VER=${ORB_DOCKER_VER_new}" >> $VAR_FILE_PATH
+
+    echo -e "${LGREEN} File 'variables.sh' was saved. Sourcing again. ${ENDCOLOR}"
+    source $VAR_FILE_PATH
+fi
 
 sudo xhost +local:root
-dockerVersion="talzzz/orbslam3:v24_10_27_working"
-echo -e "${BOLDGREEN}\n Running container ${ENDCOLOR}(${BOLDWHITE}${dockerVersion}${ENDCOLOR}) \n${ENDCOLOR}"
+dockerVersion="talzzz/${ORB_DOCKER_VER}"
+echo -e "${BOLDGREEN}\n Running container ${ENDCOLOR}(${BOLDWHITE}${dockerVersion}${ENDCOLOR}) --> ${ORB_DOCKER_CNAME} \n${ENDCOLOR}"
 
 echo -e "${BOLDGREEN}Docker VM instruction${ENDCOLOR}"
+echo -e "${BOLDGREEN}‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾${ENDCOLOR}"
 echo -e "${BOLDGREEN}1. > . ~/catkin_ws/devel/setup.bash${ENDCOLOR}"
 echo -e "${BOLDGREEN}2. > roslaunch orb_slam3_ros_wrapper euroc_monoimu.launch (make sure you see ORB viewer)${ENDCOLOR}"
 echo -e "${BOLDGREEN}3. now run  (make sure you see ORB viewer)${ENDCOLOR}"
 echo -e "${BOLDGREEN}running another inctance > sudo docker exec -it orb-3-container bash${ENDCOLOR}"
+echo -e "${BOLDGREEN}\n${ENDCOLOR}"
 
 #sudo docker run --privileged --name orb-3-container --rm -p 8087:8087 -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev:ro --gpus all -it ${dockerVersion}
-sudo docker run --privileged --name orb-3-container --rm --net=host -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev:ro --gpus all -it ${dockerVersion}
+sudo docker run --privileged --name ${ORB_DOCKER_CNAME} --rm --net=host --hostname dockerVM.${ORB_DOCKER_VER} -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev:ro --gpus all -it ${dockerVersion}
 
 # commit and push
 # sudo docker login
 # sudo docker ps    # will list all containers  
 # sudo docker commit [container-name] talzzz/orbslam3:v24_mm_dd
 # sudo docker push talzzz/orbslam3:v24_mm_dd
+
+# OR - one liner:
+# export ORB_DOCKER_NAME="orb-3-container"; export ORB_DOCKER_VER="orbslam3:v24_11_05"; sudo docker commit $ORB_DOCKER_NAME talzzz/$ORB_DOCKER_VER; sudo docker push talzzz/$ORB_DOCKER_VER
